85dbdae8be20e2fee490034a36fb79f5edf5c84a
shinlnh <kusin0412@gmail.com>
2026-09-19 22:29:42 +0700

    refactor(dsa): update binary and hash search

--- search_array/src/binary_search.cpp
+++ search_array/src/binary_search.cpp
@@ -1,22 +1,20 @@
 #include "binary_search.hpp"
 
-int Solution::search(std::vector<int>& nums, int target) {
+int Solution::binary_search(std::vector<int>& nums, int target) {
     int left = 0;
     int right = static_cast<int>(nums.size()) - 1;
 
     while (left <= right) {
-        const int mid = left + (right - left) / 2;
+        int mid = left + (right - left) / 2;
 
         if (nums[mid] == target) {
             return mid;
-        }
-        if (nums[mid] < target) {
+        } else if (nums[mid] < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
-
     return -1;
 }
 
