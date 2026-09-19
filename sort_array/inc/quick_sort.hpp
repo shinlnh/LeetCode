b0d9fc302e39afd8edffa0b6010f644fccb88841
shinlnh <kusin0412@gmail.com>
2026-09-19 17:28:46 +0700

    fix(dsa): add feature quick sort into sort array

--- sort_array/inc/quick_sort.hpp
+++ sort_array/inc/quick_sort.hpp
@@ -4,5 +4,7 @@
 
 class Solution {
 public:
+    int partition(std::vector<int>& nums, int low, int high);
+    void quickSort(std::vector<int>& nums, int low, int high);
     std::vector<int> sortArray(std::vector<int>& nums);
 };
--- sort_array/src/quick_sort.cpp
+++ sort_array/src/quick_sort.cpp
@@ -1,6 +1,29 @@
 #include "quick_sort.hpp"
 
+
+int Solution::partition(std::vector<int>& nums, int low, int high) {
+    int pivot = nums[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (nums[j] < pivot) {
+            i++;
+            std::swap(nums[i], nums[j]);
+        }
+    }
+    std::swap(nums[i + 1], nums[high]);
+    return i + 1;
+}
+
+void Solution::quickSort(std::vector<int>& nums, int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(nums, low, high);
+        quickSort(nums, low, pivotIndex - 1);
+        quickSort(nums, pivotIndex + 1, high);
+    }
+}   
 std::vector<int> Solution::sortArray(std::vector<int>& nums) {
     // TODO: Cai dat Quick Sort tai day.
+    quickSort(nums, 0, nums.size() - 1);
     return nums;
 }
